ec640439fe38bb46cb4fddb4b20498c5b5bef5e8
angel-ruga <angel_ruga@outlook.com>
2025-07-09 18:25:17 -0600

    Added haptics to the ToDo completion

--- ToDoAssistant/Editing/ToDoViewToolbar.swift
+++ ToDoAssistant/Editing/ToDoViewToolbar.swift
@@ -5,12 +5,14 @@
 //  Created by Angel Efrain Ruiz Garcia on 05/07/25.
 //
 
+import CoreHaptics
 import SwiftUI
 
 /// The toolbar for the ToDoView
 struct ToDoViewToolbar: View {
     @Environment(DataController.self) private var dataController
     @State var toDo: ToDo
+    @State private var engine = try? CHHapticEngine()
 
     var openCloseButtonText: LocalizedStringKey {
         toDo.toDoCompleted ? "Re-open ToDo" : "Complete ToDo"
@@ -24,10 +26,7 @@ struct ToDoViewToolbar: View {
                 Label("Copy ToDo Title", systemImage: "doc.on.doc")
             }
 
-            Button {
-                toDo.toDoCompleted.toggle()
-                dataController.save()
-            } label: {
+            Button(action: toggleCompleted) {
                 Label(openCloseButtonText, systemImage: "bubble.left.and.exclamationmark.bubble.right")
             }
 
@@ -39,6 +38,46 @@ struct ToDoViewToolbar: View {
 
         } label: {
             Label("Actions", systemImage: "ellipsis.circle")
+        }
+    }
+
+    func toggleCompleted() {
+        toDo.toDoCompleted.toggle()
+        dataController.save()
+
+        if toDo.toDoCompleted {
+            // UINotificationFeedbackGenerator().notificationOccurred(.success)
+            do {
+                try engine?.start()
+
+                let sharpness = CHHapticEventParameter(parameterID: .hapticSharpness, value: 0)
+                let intensity = CHHapticEventParameter(parameterID: .hapticIntensity, value: 1)
+                let start = CHHapticParameterCurve.ControlPoint(relativeTime: 0, value: 0)
+                let end = CHHapticParameterCurve.ControlPoint(relativeTime: 1, value: 1)
+                // use that curve to control the haptic strength
+                let parameter = CHHapticParameterCurve(
+                    parameterID: .hapticIntensityControl,
+                    controlPoints: [start, end],
+                    relativeTime: 0
+                )
+                let event1 = CHHapticEvent(
+                    eventType: .hapticTransient,
+                    parameters: [intensity, sharpness],
+                    relativeTime: 0
+                )
+                // create a continuous haptic event starting immediately and lasting one second
+                let event2 = CHHapticEvent(
+                    eventType: .hapticContinuous,
+                    parameters: [sharpness, intensity],
+                    relativeTime: 0.125,
+                    duration: 1
+                )
+                let pattern = try CHHapticPattern(events: [event2, event1], parameterCurves: [parameter])
+                let player = try engine?.makePlayer(with: pattern)
+                try player?.start(atTime: 0)
+            } catch {
+                // playing haptics didn't work, but that's okay
+            }
         }
     }
 }
